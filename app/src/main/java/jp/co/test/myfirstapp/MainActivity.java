3180f59a766e59c992b29be6514f4d9372a1316e
plutus0827 <plutus0827@gmail.com>
2016-04-19 18:14:57 +0900

    2016/04/19-Main修正

--- app/src/main/java/jp/co/test/myfirstapp/MainActivity.java
+++ app/src/main/java/jp/co/test/myfirstapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText)findViewById(R.id.edit_message);
+        EditText editText = (EditText)findViewById(R.id.txt_email);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
