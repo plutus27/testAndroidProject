2b794be0b3956a1434512749207e7486f3ae2291
plutus0827 <plutus0827@gmail.com>
2016-04-21 18:00:43 +0900

    2016/04/21修正

--- app/src/main/java/jp/co/test/myfirstapp/MainActivity.java
+++ app/src/main/java/jp/co/test/myfirstapp/MainActivity.java
@@ -1,9 +1,11 @@
 package jp.co.test.myfirstapp;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
     public void sendMessage(View view) {
